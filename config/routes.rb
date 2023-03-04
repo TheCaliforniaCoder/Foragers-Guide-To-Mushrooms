12b97e588fc42af2fe359c9b65141820cca5ea54
Deja_Gordon <dejagordon80@GMAIL.COM>
2023-03-04 18:40:41 +0100

    added index route for mushrooms

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :mushrooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
- root "users#index"
+ root "mushrooms#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
