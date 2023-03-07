fb603f860be38f412e467e4c92aeaebcc038b2bd
Deja_Gordon <dejagordon80@GMAIL.COM>
2023-03-07 19:44:21 +0100

    added link to home page from mushrooms index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
  
   get 'login', to: 'sessions#new'
+  get '/users/home', to: 'users#home', as: 'users_home'
   post 'login', to: 'sessions#create'
   delete '/users/:id', to: 'users#destroy'
   resources :users, only: [:index, :show]
