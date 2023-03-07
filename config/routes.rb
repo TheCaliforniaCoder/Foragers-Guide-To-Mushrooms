5ef9a4fce276deb6f9aa125a25880ca9d5b0bd0e
Deja_Gordon <dejagordon80@GMAIL.COM>
2023-03-07 13:21:52 +0100

    added see mushrooms button trying to get it to link

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get '/users/:id', to: 'users#show'
   delete '/users/:id', to: 'users#destroy'
 
 
-  
+  # routes.rb
+resources :users, only: [:index, :show]
   resources :mushrooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  root "users#index"
