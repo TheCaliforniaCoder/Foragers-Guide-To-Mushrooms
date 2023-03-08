7a44ee01dde9ed8672fdf9fcda208b371e3b2806
Deja_Gordon <dejagordon80@GMAIL.COM>
2023-03-08 09:56:07 +0100

    added images to show and edit page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/users/home', to: 'users#home', as: 'users_home'
   post 'login', to: 'sessions#create'
   delete '/users/:id', to: 'users#destroy'
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :users_home]
 
   resources :mushrooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
